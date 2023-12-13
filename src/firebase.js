f7abb3fea2f9dc7a09646f4b396e50dabf175503
Ritesh Patil <83896563+coder-Rit@users.noreply.github.com>
2023-12-13 14:07:57 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5hQ2vyepyYcYpAMhgjxFFF1AF1d3M3HU",
+  authDomain: "e-estate-bf292.firebaseapp.com",
+  projectId: "e-estate-bf292",
+  storageBucket: "e-estate-bf292.appspot.com",
+  messagingSenderId: "759000375758",
+  appId: "1:759000375758:web:d9e3199b8a2831764fcb9f"
+};
+
+// Initialize Firebase 
+export const app = initializeApp(firebaseConfig);
